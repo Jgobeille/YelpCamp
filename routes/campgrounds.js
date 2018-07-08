bd822db45891126384e741e27768e7c20c36ff69
Jamie Gobeille <djent34@gmail.com>
2018-07-08 16:00:09 +0000

    try to fix error log

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -26,7 +26,7 @@ var cloudinary = require('cloudinary');
 cloudinary.config({
     cloud_name: 'jgobeille1',
     api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
  
 var options = {
